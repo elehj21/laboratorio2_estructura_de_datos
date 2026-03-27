8934e91ddf4411bf098cd4e4a8d15c0d675d5390
Your Name <you@example.com>
2026-03-27 00:58:02 +0000

    new try

--- list.c
+++ list.c
@@ -56,7 +56,15 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-    return NULL;
+    if(list!=NULL){
+        if(list->head != NULL){
+            list->current = list->head->next;
+            *void dato = list->current->data;
+            return dato;
+        }
+        else return NULL;
+    }
+    else return NULL;
 }
 
 // 3. Programe las funciones void * lastList(List * list) y void * prevList(List * list).
